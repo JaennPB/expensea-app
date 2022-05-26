b5806096e48d12de14bdeccda77688f5947bafda
JaennPB <jaenn.bucio@gmaill.com>
2022-05-26 00:41:23 -0500

    added buttons on manage page

--- src/screens/ManageDataScreen.tsx
+++ src/screens/ManageDataScreen.tsx
@@ -2,6 +2,7 @@ import { View, Text } from "react-native";
 import React from "react";
 
 import { useAppRoute, useAppNavigation } from "../hooks/navigationHooks";
+import { Button } from "native-base";
 
 type Props = {};
 
@@ -11,9 +12,24 @@ const ManageDataScreen: React.FC = (props: Props) => {
   const mealId = route.params?.itemId;
   const isEditing = !!mealId;
 
+  function deleteItemHandler(): void {
+    navigation.goBack();
+  }
+
   React.useLayoutEffect(() => {
     navigation.setOptions({
       headerTitle: isEditing ? "Edit" : "Add",
+      headerRight: () =>
+        isEditing && (
+          <Button
+            variant="ghost"
+            _text={{ color: "error.400" }}
+            size="lg"
+            onPress={deleteItemHandler}
+          >
+            Delete
+          </Button>
+        ),
     });
   }, [navigation, isEditing]);
 
