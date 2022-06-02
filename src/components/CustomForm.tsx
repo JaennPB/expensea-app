dea74addd7e4229d0b044e2d5b94f307c7359a4c
JaennPB <jaenn.bucio@gmaill.com>
2022-06-02 00:06:28 -0500

    form completed and code refactored

--- src/components/CustomForm.tsx
+++ src/components/CustomForm.tsx
@@ -1,15 +1,25 @@
 import React from "react";
-import { VStack } from "native-base";
+import { VStack, Flex, Button, Heading, IconButton, Icon } from "native-base";
+
+import { Entypo } from "@expo/vector-icons";
+
+import moment from "moment";
 
 import CustomInput from "./UI/CustomInput";
 
-interface dataObj {
-  title: string;
-  amount: string;
+import { useAppNavigation } from "../hooks/navigationHooks";
+import { useAppDispatch } from "../hooks/reduxHooks";
+
+interface Props {
+  isEditing: boolean;
 }
 
-const CustomForm: React.FC = () => {
-  const [data, setData] = React.useState<dataObj>({
+const CustomForm: React.FC<Props> = (props: Props) => {
+  const navigation = useAppNavigation();
+  const dispatch = useAppDispatch();
+  const [isExpense, setIsExpense] = React.useState<boolean>(true);
+  const [isIncome, setIsIncome] = React.useState<boolean>(false);
+  const [data, setData] = React.useState<{ title: string; amount: string }>({
     title: "",
     amount: "",
   });
@@ -26,21 +36,95 @@ const CustomForm: React.FC = () => {
     });
   }
 
+  function submitDataHandler(data: { title: string; amount: string }): void {
+    const modifiedDataObject = {
+      id: data.title + "-id",
+      title: data.title,
+      amount: +data.amount,
+      date: moment().format("MMM Do YY"),
+      type: isExpense ? "expense" : "income",
+    };
+
+    navigation.goBack();
+    console.log(modifiedDataObject);
+  }
+
+  function toggleExpenseOrIncomeHandler(dataType: string): void {
+    if (dataType === "expense" && !isExpense) {
+      setIsExpense(true);
+      setIsIncome(false);
+    }
+
+    if (dataType === "income" && !isIncome) {
+      setIsIncome(true);
+      setIsExpense(false);
+    }
+  }
+
+  let topContent!: JSX.Element;
+  let headingContent!: string;
+
+  if (!props.isEditing) {
+    topContent = (
+      <Button.Group
+        isAttached
+        borderWidth={1}
+        borderRadius={5}
+        borderColor="darkBlue.600"
+      >
+        <IconButton
+          icon={<Icon as={Entypo} name="minus" color="white" />}
+          bgColor={isExpense ? "darkBlue.600" : null}
+          w={66}
+          onPress={toggleExpenseOrIncomeHandler.bind(this, "expense")}
+        />
+        <IconButton
+          icon={<Icon as={Entypo} name="plus" color="white" />}
+          bgColor={isIncome ? "darkBlue.600" : null}
+          w={66}
+          onPress={toggleExpenseOrIncomeHandler.bind(this, "income")}
+        />
+      </Button.Group>
+    );
+
+    if (isExpense) headingContent = "New expense";
+    if (isIncome) headingContent = "New income";
+  }
+
   return (
-    <VStack w="100%" mt={5} space={5}>
-      <CustomInput
-        title="Title"
-        type="default"
-        onChangeText={dataEnteredHandler.bind(this, "title")}
-        value={data.title}
-      />
-      <CustomInput
-        title="Amount"
-        type="decimal-pad"
-        onChangeText={dataEnteredHandler.bind(this, "amount")}
-        value={data.amount}
-      />
-    </VStack>
+    <>
+      {topContent}
+      <Heading color="white" mt={props.isEditing ? 0 : 5}>
+        {headingContent}
+      </Heading>
+
+      <VStack w="100%" mt={props.isEditing ? 0 : 5} space={5}>
+        <CustomInput
+          title="Title"
+          type="default"
+          onChangeText={dataEnteredHandler.bind(this, "title")}
+          value={data.title}
+        />
+        <CustomInput
+          title="Amount"
+          type="decimal-pad"
+          onChangeText={dataEnteredHandler.bind(this, "amount")}
+          value={data.amount}
+        />
+      </VStack>
+      <Flex direction="row" w="100%" mt={5} justify="space-between">
+        <Button w={150} variant="ghost" onPress={() => navigation.goBack()}>
+          Cancel
+        </Button>
+        <Button
+          w={150}
+          bg="success.400"
+          onPress={submitDataHandler.bind(this, data)}
+        >
+          {props.isEditing ? "Update" : "Add"}
+        </Button>
+      </Flex>
+    </>
   );
 };
 
--- src/screens/ManageDataScreen.tsx
+++ src/screens/ManageDataScreen.tsx
@@ -1,53 +1,26 @@
-import { Button, IconButton, Icon, Flex, Heading } from "native-base";
+import { Button, Flex } from "native-base";
 import React from "react";
 
 import CustomForm from "../components/CustomForm";
 
-import { Entypo } from "@expo/vector-icons";
-
 import { useAppRoute, useAppNavigation } from "../hooks/navigationHooks";
-import { DUMMY_DATA } from "../../DUMMY_DATA";
 
 const ManageDataScreen: React.FC = () => {
   const route = useAppRoute();
   const navigation = useAppNavigation();
-  const [isExpense, setIsExpense] = React.useState<boolean>(true);
-  const [isIncome, setIsIncome] = React.useState<boolean>(false);
   const mealId = route.params?.itemId;
-  const mealType = DUMMY_DATA.filter((el) => el.id === mealId);
   const isEditing = !!mealId;
 
   function deleteItemHandler(): void {
     navigation.goBack();
   }
 
-  function toggleExpenseOrIncomeHandler(dataType: string): void {
-    if (dataType === "expense" && !isExpense) {
-      setIsExpense(true);
-      setIsIncome(false);
-    }
-
-    if (dataType === "income" && !isIncome) {
-      setIsIncome(true);
-      setIsExpense(false);
-    }
-  }
-
   React.useLayoutEffect(() => {
     navigation.setOptions({
       headerTitle: isEditing ? "Edit" : "Add",
-      headerLeft: () => (
-        <Button
-          variant="ghost"
-          _text={{ color: "error.400" }}
-          size="lg"
-          onPress={() => navigation.goBack()}
-        >
-          Cancel
-        </Button>
-      ),
+      headerBackVisible: false,
       headerRight: () =>
-        isEditing ? (
+        isEditing && (
           <Button
             variant="ghost"
             _text={{ color: "error.400" }}
@@ -56,56 +29,19 @@ const ManageDataScreen: React.FC = () => {
           >
             Delete
           </Button>
-        ) : (
-          <Button
-            variant="ghost"
-            _text={{ color: "darkBlue.600" }}
-            size="lg"
-            onPress={deleteItemHandler}
-          >
-            Save
-          </Button>
         ),
     });
   }, [navigation, isEditing]);
 
-  let topContent!: JSX.Element;
-  let headingContent!: string;
-
-  if (!isEditing) {
-    topContent = (
-      <Button.Group
-        isAttached
-        borderWidth={1}
-        borderRadius={5}
-        borderColor="darkBlue.600"
-      >
-        <IconButton
-          icon={<Icon as={Entypo} name="minus" color="white" />}
-          bgColor={isExpense ? "darkBlue.600" : null}
-          w={66}
-          onPress={toggleExpenseOrIncomeHandler.bind(this, "expense")}
-        />
-        <IconButton
-          icon={<Icon as={Entypo} name="plus" color="white" />}
-          bgColor={isIncome ? "darkBlue.600" : null}
-          w={66}
-          onPress={toggleExpenseOrIncomeHandler.bind(this, "income")}
-        />
-      </Button.Group>
-    );
-
-    if (isExpense) headingContent = "New expense";
-    if (isIncome) headingContent = "New income";
-  }
-
   return (
-    <Flex flex={1} bg="darkBlue.700" align="center" p={5}>
-      {topContent}
-      <Heading color="white" mt={5}>
-        {headingContent}
-      </Heading>
-      <CustomForm />
+    <Flex
+      flex={1}
+      bg="darkBlue.700"
+      align="center"
+      pt={isEditing ? 0 : 5}
+      px={5}
+    >
+      <CustomForm isEditing={isEditing} />
     </Flex>
   );
 };
